46658fe4527b76f7eb72d14aa4b7a5c18683c4e9
Stefan Wolfsheimer <stefan.wolfsheimer@gmail.com>
2017-10-08 13:34:50 +0200

    fixed flake8

--- python/list/map-reduce-filter.py
+++ python/list/map-reduce-filter.py
@@ -4,6 +4,6 @@ print "a=" + str(a)
 print "b=" + str(b)
 
 a_pow_2 = map(lambda x: pow(x, 2), a)
-a_plus_b = map(lambda x,y: x+y, a, b)
+a_plus_b = map(lambda x, y: x+y, a, b)
 print "a^2=" + str(a_pow_2)
 print "a+b=" + str(a_plus_b)
