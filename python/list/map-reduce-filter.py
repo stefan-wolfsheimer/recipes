cdb0aed1fa22b22a27537c0766bbeae9034e2922
Stefan Wolfsheimer <stefan.wolfsheimer@gmail.com>
2017-10-08 13:30:23 +0200

    added map example

---
+++ python/list/map-reduce-filter.py
@@ -0,0 +1,9 @@
+a = [1, 2, 3, 4]
+b = [4, 3, 2, 1]
+print "a=" + str(a)
+print "b=" + str(b)
+
+a_pow_2 = map(lambda x: pow(x, 2), a)
+a_plus_b = map(lambda x,y: x+y, a, b)
+print "a^2=" + str(a_pow_2)
+print "a+b=" + str(a_plus_b)
