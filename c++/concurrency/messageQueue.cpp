6c7584448058eb3e7b2fb7af17b3064afdb1664b
Stefan Wolfsheimer <stefan.wolfsheimer@gmail.com>
2017-10-25 23:32:38 +0200

    added message Queue

---
+++ c++/concurrency/messageQueue.cpp
@@ -0,0 +1,58 @@
+#include <string>
+#include <queue>
+
+#include <iostream>
+
+#include <mutex>
+#include <thread>
+#include <condition_variable>
+
+std::mutex queueMutex;
+std::queue<std::string> messageQueue;
+std::condition_variable conditionVariable;
+
+void consumer()
+{
+  bool run = true;
+  while(run)
+  {
+    std::unique_lock<std::mutex> lock(queueMutex);
+    conditionVariable.wait(lock);
+    while(!messageQueue.empty())
+    {
+      auto message = messageQueue.front();
+      messageQueue.pop();
+      if(!message.empty())
+      {
+        std::cout << message << std::endl;
+      }
+      else
+      {
+        run = false;
+      }
+    }
+    lock.unlock();
+  }
+}
+
+void producer()
+{
+  for(std::size_t i = 0; i < 10000; i++)
+  {
+    std::unique_lock<std::mutex> lock(queueMutex);
+    messageQueue.push(std::string("Task ") + std::to_string(i));
+    conditionVariable.notify_one();
+  }
+  std::unique_lock<std::mutex> lock(queueMutex);
+  messageQueue.push("");
+  conditionVariable.notify_one();
+}
+
+int main(int argc, char ** argv)
+{
+  std::thread consumerThread(consumer);
+  producer();
+  consumerThread.join();
+  return 0;
+}
+
